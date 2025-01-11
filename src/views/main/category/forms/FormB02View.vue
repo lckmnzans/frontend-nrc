aacfd9af199ee98a629305cdfc431d8395b706cd
Lukman Sanusi <lckmnzans@gmail.com>
2025-01-11 20:56:19 +0700

    utilizing shared FormOptsConfig

--- src/views/main/category/forms/FormB02View.vue
+++ src/views/main/category/forms/FormB02View.vue
@@ -31,7 +31,7 @@
                     <label for="" class="form-label">Tujuan Surat *</label>
                     <input type="text" class="form-control" v-model="docData.tujuanSurat" />
                 </div>
-                <div class="alert alert-info" role="alert">
+                <div class="alert alert-info" role="alert" v-if="ocrable">
                     Perhatian! Form yang dikosongkan akan diisi otomatis oleh sistem
                 </div>
             </form>
@@ -50,29 +50,14 @@
 import Loading from '@/components/Loading.vue';
 import PdfForm from '@/components/PdfForm.vue';
 import PreviewPdf from '@/components/PreviewPdf.vue';
-import api from '@/api/document.api';
-import { useToastStore } from '@/store/toastStore';
-import { mapActions } from 'pinia';
+import FormOptions from '@/views/main/category/forms/FormOptsConfig';
+
 export default {
+    ...FormOptions,
     components: {
         Loading,
         PdfForm,
         PreviewPdf
-    },
-    props: {
-        mode: {
-            type: String,
-            default: 'create',
-            validator: (value) => ['create', 'edit'].includes(value)
-        },
-        docId: {
-            type: String,
-            required: false
-        },
-        filename: {
-            type: String,
-            required: false
-        }
     },
     created() {
         this.fetchData();
@@ -84,8 +69,7 @@ export default {
     },
     data() {
         return {
-            localPreview: null,
-            selectedFile: null,
+            ...FormOptions.data(),
             docType: 'B02',
             docData: {
                 noSurat: '',
@@ -94,121 +78,8 @@ export default {
                 drafter: '',
                 tujuanSurat: ''
             },
-            loading: false,
-            error: false,
+            ocrable: true,
         };
-    },
-    methods: {
-        async handleUpdate() {
-            this.axios(api.updateDocData(this.docData, this.docType, this.docId))
-            .then(response => {
-                if (response.status == 200) {
-                    const body = response.data;
-                    this.setToast('', 'Dokumen berhasil diperbarui', 3000);
-                    this.$router.back();
-                } else {
-                    this.setToast('', 'Dokumen gagal diperbarui', 3000);
-                }
-            })
-            .catch(err => {
-                console.log('Permintaan tidak bisa diproses. Error: ' + err);
-            })
-        },
-        async handleSubmit(file) {
-            if (this.isRequiredFormEmpty) {
-                this.setToast('', 'Form ada yang perlu diisi', 3000);
-            } else {
-                this.uploadFile(file);
-            }
-        },
-        async uploadFile(file) {
-            if (!file) {
-                alert('Tidak ada file yang dipilih.');
-                return;
-            }
-
-            this.axios(api.upload(file, this.docType))
-            .then(response => {
-                if (response.status == 200) {
-                    const body = response.data;
-                    const data = {
-                        docName: body.data.file.filename,
-                        docType: body.data.file.documentType,
-                        ...this.docData
-                    };
-                    this.selectedFile = null;
-                    this.uploadDocument(data);
-                } else {
-                    console.log('File gagal diunggah.');
-                }
-            })
-            .catch(error => {
-                console.log('Permintaan tidak bisa diproses. Error: ' + error);
-            })
-        },
-        async uploadDocument(data) {
-            this.axios(api.uploadDocData(data, data.docType))
-            .then(response => {
-                if (response.status == 200) {
-                    const body = response.data;
-                    console.log('Dokumen berhasil diunggah');
-                    this.setToast('', 'Dokumen berhasil diunggah.', 3000);
-                } else {
-                    const body = response.data;
-                    console.log('Dokumen gagal diunggah.');
-                }
-            })
-            .catch(error => {
-                console.log('Permintaan tidak bisa diproses. Error: ' + error);
-            })
-        },
-        async fetchData() {
-            if (this.docId) {
-                this.axios(api.getDocData(this.docId))
-                .then(response => {
-                    if (response.status == 200) {
-                        const body = response.data;
-                        console.log('Dokumen berhasil diambil');
-                        this.fetchFile(body.data.docName);
-                    } else {
-                        const body = response.data;
-                        console.log('Dokumen gagal diambil. Error: ' + body.message);
-                    }
-                })
-                .catch(err => {
-                    console.log('Permintaan tidak bisa diproses. Error: ' + err)
-                })
-            }
-        },
-        async fetchFile(filename) {
-            this.error = false;
-            this.axios(api.getDocFile(filename))
-            .then(response => {
-                if (response.status == 200) {
-                    const body = response.data;
-                    console.log('File berhasil diambil');
-                    this.localPreview = URL.createObjectURL(body);
-                    this.error = false;
-                } else {
-                    const body = response.data;
-                    console.log('File gagal diambil. Error: ' + body.message);
-                    this.localPreview = null;
-                    this.error = true;
-                }
-            })
-            .catch(err => {
-                console.log('Permintaan tidak bisa diproses. Error: ' + err);
-                this.error = true;
-            })
-        },
-        ...mapActions(useToastStore, {
-            setToast: 'setToast'
-        })
-    },
-    beforeUnmount() {
-        if (this.localPreview) {
-            URL.revokeObjectURL(this.localPreview);
-        }
     }
 }
 </script>
---
+++ src/views/main/category/forms/FormOptsConfig.js
@@ -0,0 +1,138 @@
+// sharedComponentConfig.js
+import api from '@/api/document.api';
+import { useToastStore } from '@/store/toastStore';
+import { mapActions } from 'pinia';
+
+export default {
+    props: {
+        mode: {
+            type: String,
+            default: 'create',
+            validator: (value) => ['create', 'edit'].includes(value),
+        },
+        docId: {
+            type: String,
+            required: false,
+        },
+        filename: {
+            type: String,
+            required: false,
+        },
+    },
+    data() {
+        return {
+            localPreview: null,
+            selectedFile: null,
+            loading: false,
+            error: false,
+        };
+    },
+    methods: {
+        async handleUpdate() {
+            this.axios(api.updateDocData(this.docData, this.docType, this.docId))
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.setToast('', 'Dokumen berhasil diperbarui', 3000);
+                        this.$router.back();
+                    } else {
+                        this.setToast('', 'Dokumen gagal diperbarui', 3000);
+                    }
+                })
+                .catch((err) => {
+                    console.error('Error:', err);
+                });
+        },
+        async handleSubmit(file) {
+            if (this.isRequiredFormEmpty) {
+                this.setToast('', 'Form ada yang perlu diisi', 3000);
+            } else {
+                this.uploadFile(file);
+            }
+        },
+        async uploadFile(file) {
+            if (!file) {
+                alert('Tidak ada file yang dipilih.');
+                return;
+            }
+
+            this.axios(api.upload(file, this.docType))
+                .then((response) => {
+                    if (response.status === 200) {
+                        const body = response.data;
+                        const data = {
+                            docName: body.data.file.filename,
+                            docType: body.data.file.documentType,
+                            ...this.docData,
+                        };
+                        this.selectedFile = null;
+                        this.uploadDocument(data);
+                    } else {
+                        console.error('File gagal diunggah.');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        },
+        async uploadDocument(data) {
+            this.axios(api.uploadDocData(data, data.docType))
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.setToast('', 'Dokumen berhasil diunggah.', 3000);
+                    } else {
+                        console.error('Dokumen gagal diunggah.');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        },
+        async fetchData() {
+            if (this.docId) {
+                this.axios(api.getDocData(this.docId))
+                    .then((response) => {
+                        if (response.status === 200) {
+                            const body = response.data;
+                            Object.keys(this.docData).forEach((key) => {
+                                if (body.data.hasOwnProperty(key)) {
+                                    this.docData[key] = body.data[key];
+                                }
+                            });
+                            this.fetchFile(body.data.docName);
+                        } else {
+                            console.error('Dokumen gagal diambil.');
+                        }
+                    })
+                    .catch((err) => {
+                        console.error('Error:', err);
+                    });
+            }
+        },
+        async fetchFile(filename) {
+            this.error = false;
+            this.axios(api.getDocFile(filename))
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.localPreview = URL.createObjectURL(response.data);
+                        this.error = false;
+                    } else {
+                        console.error('File gagal diambil.');
+                        this.localPreview = null;
+                        this.error = true;
+                    }
+                })
+                .catch((err) => {
+                    console.error('Error:', err);
+                    this.error = true;
+                });
+        },
+        ...mapActions(useToastStore, {
+            setToast: 'setToast',
+        }),
+    },
+    beforeUnmount() {
+        if (this.localPreview) {
+            URL.revokeObjectURL(this.localPreview);
+        }
+    },
+};
